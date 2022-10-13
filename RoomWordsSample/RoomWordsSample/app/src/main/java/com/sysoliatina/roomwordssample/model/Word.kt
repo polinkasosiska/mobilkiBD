c90ee13235e76e0039450207e39de3e474c7d192
polinkasosiska <polina.sysoliatina@gmail.com>
2022-10-13 19:18:20 +0300

    .

--- RoomWordsSample/RoomWordsSample/app/src/main/java/com/sysoliatina/roomwordssample/model/Word.kt
+++
@@ -1,9 +0,0 @@
-package com.sysoliatina.roomwordssample.model
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity()
-class Word(val word: String) {
-    @PrimaryKey(autoGenerate = true) var id: Int = 0
-}
